0a4b45e9396817b4de351c1d2c453fca071bfc27
NeonN9 <hawarmustafa123@gmail.com>
2021-11-30 15:49:42 +0300

    removed extra codes

--- lib/screens/forgot_password.dart
+++ lib/screens/forgot_password.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:jobfinder/provider/auth.dart';
 import 'package:jobfinder/widgets/error_dialog.dart';
 import 'package:jobfinder/widgets/signin_widgets.dart';
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -35,7 +35,7 @@ class _HomeState extends State<Home> {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (_) => SignIn(),
+                  builder: (_) => const SignIn(),
                 ),
               );
             }),
